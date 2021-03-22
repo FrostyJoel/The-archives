9f7acd8155b20c1a148e8aefc5f26f2f3a37207c
Joël John <JsmJohn@hotmail.com>
2021-03-22 14:28:21 +0100

    Fun Setup For Camera's

---
+++ TheArchives/Assets/Editor/RenderObtainerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RandomColorPicker))]
+public class RenderObtainerEditor : Editor
+{
+#if UNITY_EDITOR
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RandomColorPicker myScript = (RandomColorPicker)target;    
+        if(GUILayout.Button("Get Renderes"))
+        {
+            myScript.ObtainRenderes();
+        }
+
+        if(GUILayout.Button("Start ChangeGame"))
+        {
+            myScript.StartSwap();
+        }
+
+        if (GUILayout.Button("Stop ChangeGame"))
+        {
+            myScript.StopSwap();
+        }
+    }
+#endif
+}
---
+++ TheArchives/Assets/Programming/PlayerController/Camera Controller/Youtube 360 Video/RandomColorPicker.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColorPicker : MonoBehaviour
+{
+    [Header ("Color Changer")]
+    [SerializeField] private MeshRenderer[] render;
+    [SerializeField] private Material[] allMats;
+    [SerializeField] private float colorTimeDelay = 2f;
+    [Header("Jump Changer")]
+    [SerializeField] private float upDraftAmount = 1f;
+    [SerializeField] private float jumpTimeDelay = 3f;
+    
+    public void ObtainRenderes()
+    {
+        render = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    public void StartSwap()
+    {
+        if (render.Length > 0)
+        {
+            StartCoroutine(ColorChange());
+            StartCoroutine(Uplift());
+        }
+        else
+        {
+            Debug.Log("No Renders Assgined");
+        }
+    }
+
+    public void StopSwap()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator Uplift()
+    {
+        while (true)
+        {
+            foreach (MeshRenderer r in render)
+            {
+                int chance = Random.Range(0, 2);
+                if(chance == 0)
+                {
+                    Vector3 upLift = new Vector3(r.transform.position.x, r.transform.position.y + upDraftAmount, r.transform.position.z);
+                    r.transform.position = upLift;
+                }
+            }
+            yield return new WaitForSeconds(jumpTimeDelay);
+        }
+    }
+
+    IEnumerator ColorChange()
+    {
+        while (true)
+        {
+            foreach (MeshRenderer r in render)
+            {
+                Material randomMat = allMats[Random.Range(0, allMats.Length)];
+                r.material = randomMat;
+            }
+            yield return new WaitForSeconds(colorTimeDelay);
+        }
+    }
+}
